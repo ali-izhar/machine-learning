64ac836c5a91852c96fd3bf47956c95b0370c915
Izhar Ali <aliizh94@students.rowan.edu>
2023-06-11 17:43:50 -0400

    naive bayes probability completed

--- learning/probability/__init__.py
+++ learning/probability/__init__.py
@@ -1,5 +1,4 @@
 from .generators import Generator
-from .inverse import inverse_cdf_gaussian, inverse_cdf_binomial
 from .utils import plot_gaussian_distributions, plot_binomial_distributions
 
-__all__ = ['Generator', 'inverse_cdf_gaussian', 'inverse_cdf_binomial', 'plot_gaussian_distributions', 'plot_binomial_distributions']
+__all__ = ['Generator', 'plot_gaussian_distributions', 'plot_binomial_distributions']
--- learning/probability/generators.py
+++ learning/probability/generators.py
@@ -1,11 +1,36 @@
 import numpy as np
-from inverse import inverse_cdf_gaussian, inverse_cdf_binomial
+from scipy.special import erfinv
+from scipy.stats import binom
 
 __all__ = ['Generator']
 
 class Generator(object):
     def __init__(self):
         pass
+
+    def _inverse_cdf_gaussian(self, y, mu, sigma):
+        """
+        Calculates the inverse of the CDF of the Gaussian distribution.
+        :param y (float or ndarray): The probability or array of probabilities.
+        :param mu (float): Mean of the Gaussian distribution.
+        :param sigma (float): Standard deviation of the Gaussian distribution.
+        :return x (float or ndarray): The corresponding value(s) from the Gaussian distribution that
+        correspond to the given probability value(s).
+        """
+        x = mu + np.sqrt(2) * sigma * erfinv(2 * y - 1)
+        return x
+    
+    def _inverse_cdf_binomial(self, y, n, p):
+        """
+        Calculates the inverse of the CDF of the binomial distribution.
+        :param y (float or ndarray): The probability or array of probabilities.
+        :param n (int): Number of trials in the binomial distribution.
+        :param p (float): Probability of success in each trial.
+        :return x (float or ndarray): The corresponding value(s) from the binomial distribution that
+        correspond to the given probability value(s).
+        """
+        x = binom.ppf(y, n, p)
+        return x
 
     def uniform_generator(self, a, b, num_samples=100):
         """
@@ -32,7 +57,7 @@ class Generator(object):
         u = self.uniform_generator(0, 1, num_samples)
 
         # Use the inverse CDF technique to generate an array of normally distributed random numbers
-        array = inverse_cdf_gaussian(u, mu, sigma)
+        array = self._inverse_cdf_gaussian(u, mu, sigma)
         return array
     
     def binomial_generator(self, n, p, num_samples=100):
@@ -49,5 +74,5 @@ class Generator(object):
         u = self.uniform_generator(0, 1, num_samples)
 
         # Use the inverse CDF technique to generate an array of binomially distributed random numbers
-        array = inverse_cdf_binomial(u, n, p)
+        array = self._inverse_cdf_binomial(u, n, p)
         return array
