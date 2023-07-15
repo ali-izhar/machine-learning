f93a1ada8fc3f98144ffaec8e499a77aab54359a
Izhar Ali <aliizh94@students.rowan.edu>
2023-07-14 22:02:25 -0400

    forward prop completed

--- theory/NNs/propagation/forward.py
+++ theory/NNs/propagation/forward.py
@@ -3,39 +3,57 @@ import numpy as np
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
-def dense(A_in, W, B, g):
-    Z = np.matmul(A_in, W) + B
+def dense(AT, W, B, g):
+    """
+    Perform the dense (fully connected) layer operation.
+
+    Args:
+        AT: Input matrix A with shape (m, n_prev).
+        W: Weight matrix with shape (n_prev, n).
+        B: Bias vector with shape (1, n).
+        g: Activation function.
+
+    Returns:
+        A_out: Output matrix A with shape (m, n).
+    """
+    Z = np.dot(AT, W) + B
     A_out = g(Z)
     return A_out
 
 def sequential(X, W, B, g):
-    L = len(W)                      # number of layers
-    A = [X]                         # list of activation values
-    for l in range(L):              # for each layer
-        a_in = A[l]                 # input activation values
-        W_l = W[l]                  # weights for layer l
-        b_l = B[l]                  # bias for layer l
-        a_out = dense(a_in, W_l, b_l, g)
-        A.append(a_out)
-    return (A)
+    """
+    Perform forward propagation through a sequential neural network.
 
+    Args:
+        X: Input matrix with shape (m, n_0).
+        W: List of weight matrices.
+        B: List of bias vectors.
+        g: Activation function.
+
+    Returns:
+        A: Output matrix with shape (m, n_L).
+    """
+    A = X
+    for i in range(len(W)):
+        # A is the most recent activation matrix
+        Z = np.dot(A, W[i]) + B[i]
+        A = g(Z)
+    return A
 
 def predict(X, W, B, g):
     """
-    Computes prediction for sequential NN
+    Predict the class labels for input examples.
+
     Args:
-      X (ndarray (n, )) : Input values, n features
-      W (list (L, ))    : List of weight matrices, L layers
-      B (list (L, ))    : List of bias
-      g (function)      : Activation function
+        X: Input matrix with shape (m, n_0).
+        W: List of weight matrices.
+        B: List of bias vectors.
+        g: Activation function.
+
     Returns:
-      y_pred (ndarray (j, )) : Prediction, j units
+        predictions: Array of predicted class labels.
     """
-    m = X.shape[0]                  # number of samples
-    p = np.zeros((m, 1))            # prediction
-    for i in range(m):              # for each sample
-        x = X[i,:]                  # ith sample
-        A = sequential(x, W, B, g)  # forward propagation
-        y_pred = A[-1]              # prediction
-        p[i] = y_pred               # store prediction
-    return (p)
+    A = sequential(X, W, B, g)
+    # Predict the class with the highest probability
+    predictions = np.argmax(A, axis=1)
+    return predictions
