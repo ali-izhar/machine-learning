81faad36587f9cac71eb09da7ef0c0413ebdaaba
Izhar Ali <aliizh94@students.rowan.edu>
2023-07-22 02:13:02 -0400

    TF: project digit classifier updated

--- Theory/Neural_Networks/Multiclass_Classification/Softmax/regressor.py
+++ Theory/Neural_Networks/Multiclass_Classification/Softmax/regressor.py
@@ -5,4 +5,5 @@ class SoftmaxRegressor:
     def softmax(self, z):
         return np.exp(z) / np.sum(np.exp(z), axis=1, keepdims=True)
     
-    
+    def cross_entropy(self, y, y_hat):
+        return - np.sum(y * np.log(y_hat)) / y.shape[0]
